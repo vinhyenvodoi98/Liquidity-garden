a5bd9ad0222fe389392f5dc12865cb5b58ec444c
vinhyenvodoi98 <vinhyenvodoi98@gmail.com>
2024-05-28 21:02:20 +0900

    add 60s timeout

--- src/tools/sui-indexer.ts
+++ src/tools/sui-indexer.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: `https://api.blockeden.xyz/sui-indexer/${process.env.NEXT_PUBLIC_BLOCKEDEN_API}`,
-  timeout: 5000, // Timeout if necessary
+  timeout: 60000, // Timeout if necessary
 });
 
 
